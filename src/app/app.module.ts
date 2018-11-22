858ea986727e76af91bc26bad33b2a72b5d22259
dries leeman <driesleeman0@gmail.com>
2018-11-22 14:52:12 +0100

    addes sensortag tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SensortagPage } from '../pages/Sensortag/sensortag';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    SensortagPage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    SensortagPage,
     HomePage,
     TabsPage
   ],
